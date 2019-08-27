8a6adc2ba29dc9f1ea75ab52e27479511d0a09f3
Ali Samari <alexsamarim@gmail.com>
2019-08-27 03:30:05 -0400

    updated app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatFormFieldModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatInputModule,
+  MatSortModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTabsModule,
+  MatListModule,
+  MatDividerModule,
+} from '@angular/material';
+
+import {} from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatListModule,
+    MatDividerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
